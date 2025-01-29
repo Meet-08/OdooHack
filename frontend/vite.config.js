db1292d50f569434a83e001d8b56c34a36995f22
Meet-08 <meetjbhuva@gmail.com>
2025-01-29 20:35:33 +0530

    Scheme page done

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // How to allow all host
   server: {
     host: true,
     allowedHosts: ['*']
