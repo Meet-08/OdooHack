4b23399a45734a93e309229910697cf99a0cf56e
Meet-08 <meetjbhuva@gmail.com>
2025-01-31 18:06:25 +0530

    Form Done

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -16,6 +16,8 @@ router.post('/register', async (req, res) => {
 
 router.get('/users', async (req, res) => {
     try {
+        const users = await User.find();
+        res.status(200).json(users);
     } catch (err) {
         res.status(500).json({ error: err.message });
     }
