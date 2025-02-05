c26df0a71408c6afa878d66be6f34d074b32269b
Meet-08 <meetjbhuva@gmail.com>
2025-02-05 21:45:59 +0530

    Home Login Register Done

--- frontend/src/pages/Initiative.jsx
+++ frontend/src/pages/Initiative.jsx
@@ -2,17 +2,19 @@ import React from 'react'
 import Card from '../components/Card';
 import Sidebar from '../components/Sidebar';
 import Mstyle from '../style/main.module.css';
-
+import Topbar from '../components/Topbar'
 import RightBar from '../components/RightBar';
 
 
 function initiative() {
     return (
         <div>
+            <Topbar />
             <div className={`${Mstyle.maindiv}`}>
                 <div className={Mstyle.left}>
                     <Sidebar>
                         <Card />
+                        <div className="h-full w-2.5 bg-gray-300" />
                         <RightBar />
                     </Sidebar>
                 </div>
